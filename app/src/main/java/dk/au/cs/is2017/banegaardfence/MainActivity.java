855c81a434455cdb480c3f04e45111068e07a46c
NicolajEngby <nicolaj.m.engby@hotmail.com>
2017-11-09 14:04:14 +0100

    first commit

---
+++ app/src/main/java/dk/au/cs/is2017/banegaardfence/MainActivity.java
@@ -0,0 +1,137 @@
+package dk.au.cs.is2017.banegaardfence;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Geofence;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+    implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TAG = "BanegaardFence";
+    public static final int LOCATION_REQUEST_CODE = 1;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Geofence mBanegaardFence;
+    private GeofencingRequest mRequest;
+    private PendingIntent mPi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up Google API
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "Google Play Services connected!");
+
+        // Let's create a Geofence around the Hovedbanegård
+        mBanegaardFence = new Geofence.Builder()
+                .setRequestId("hovedbanegaard")
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                .setCircularRegion(56.1503116, 10.2047365, 1500)
+                .build();
+
+        mRequest = new GeofencingRequest.Builder()
+                .addGeofence(mBanegaardFence)
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                .build();
+
+        // Create an Intent pointing to the IntentService
+        Intent intent = new Intent(this,
+                ReceiveGeoFenceTransitionService.class);
+        mPi = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            LocationServices.GeofencingApi.addGeofences(mGoogleApiClient, mRequest, mPi);
+            Log.d(TAG, "We added the geofence!");
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    LOCATION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    String message = "Location permission accepted. Geofence will be created.";
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+                    // OK, request it now
+                    LocationServices.GeofencingApi.addGeofences(mGoogleApiClient, mRequest, mPi);
+                    Log.d(TAG, "We added the geofence!");
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    String message = "Location permission denied. Geofence will not work.";
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Google Play Services connection suspended!");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "Google Play Services connection failed!");
+    }
+}
---
+++ app/src/main/java/dk/au/cs/is2017/banegaardfence/ReceiveGeoFenceTransitionService.java
@@ -0,0 +1,114 @@
+package dk.au.cs.is2017.banegaardfence;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.content.Intent;
+import android.content.Context;
+
+import android.app.PendingIntent;
+import android.net.Uri;
+import android.util.Log;
+import android.graphics.BitmapFactory;
+
+import android.os.Build;
+import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import android.app.NotificationChannel;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
+public class ReceiveGeoFenceTransitionService extends IntentService {
+
+    // Notification channel ID, needed for API 26 and higher
+    public final static String CHANNEL_ID = "default";
+
+    public ReceiveGeoFenceTransitionService() {
+        super("ReceiveGeoFenceTransitionService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+
+        if (event.hasError()) {
+            // TODO: Handle error
+        } else {
+            int transition = event.getGeofenceTransition();
+
+            if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                String transitionType = getTransitionString(transition);
+
+                Log.d(MainActivity.TAG, getString(R.string.geofence_transition_notification_title, transitionType));
+
+                // Send a notification, when clicked, open website
+                String url = "https://www.rejseplanen.dk/webapp/index.html?language=en_EN&#!S|Aarhus%20H!Z|%C3%85bogade%2034%2C%208200%20Aarhus%20N%2C%20Aarhus%20Kommune!start|1";
+                Intent notificationIntent = new Intent(Intent.ACTION_VIEW);
+                notificationIntent.setData(Uri.parse(url));
+
+                PendingIntent contentIntent = PendingIntent.getActivity(this.getApplicationContext(), 0, notificationIntent, 0);
+
+                // Create a notification channel if on API 26 (Android O) and above
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Default Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+                    // Configure the notification channel.
+                    notificationChannel.setDescription("Channel description");
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+                    notificationChannel.enableVibration(true);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+
+                Notification notification = new NotificationCompat.Builder(this.getApplicationContext(), CHANNEL_ID)
+                        .setContentTitle("Near Aarhus H!")
+                        .setContentText("Check out how to get to the Datalogi building!")
+                        .setTicker("You're near the Hovedbanegaard. Do you want to see how to get to the CS building?")
+                        .setContentIntent(contentIntent)
+                        .setSmallIcon(R.drawable.stat_sys_gps_on)
+                        .setAutoCancel(true)
+                        .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.drawable.rejse))
+                        .build();
+
+                Log.d(MainActivity.TAG, "Notification created");
+
+                NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+                manager.notify(1, notification);
+
+                Log.d(MainActivity.TAG, "Notified!");
+            } else {
+                // TODO: Handle invalid transition
+            }
+        }
+    }
+
+    /**
+     * Maps geofence transition types to their human-readable equivalents.
+     *
+     * @param transitionType A transition type constant defined in Geofence
+     * @return A String indicating the type of transition
+     */
+    private String getTransitionString(int transitionType) {
+        switch (transitionType) {
+
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return getString(R.string.geofence_transition_entered);
+
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return getString(R.string.geofence_transition_exited);
+
+            default:
+                return getString(R.string.geofence_transition_unknown);
+        }
+    }
+}
