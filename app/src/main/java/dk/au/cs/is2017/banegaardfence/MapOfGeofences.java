68b9d830d6d418b7f72b2ca56308706b48c8babb
frede1994 <fj1994@outlook.dk>
2017-11-17 12:41:04 +0100

    new map acitivty

--- app/src/main/java/dk/au/cs/is2017/banegaardfence/MapOfGeofences.java
+++ app/src/main/java/dk/au/cs/is2017/banegaardfence/MapOfGeofences.java
@@ -3,6 +3,7 @@ package dk.au.cs.is2017.banegaardfence;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,15 +11,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 public class MapOfGeofences extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
+    private ArrayList<GeofenceObjects> currentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_of_geofences);
+        currentList = new ArrayList<>();
+        readFromInternalStorage();
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -26,6 +35,47 @@ public class MapOfGeofences extends FragmentActivity implements OnMapReadyCallba
         mapFragment.getMapAsync(this);
 
 
+    }
+
+    public void addMarkerForFence(){
+        for (GeofenceObjects geofenceObjects : currentList) {
+
+            double lat =
+            mMap.addMarker( new MarkerOptions()
+                    .position( new LatLng() )
+                    .title("Fence " + fence.getId())
+                    .snippet("Radius: " + fence.getRadius()) ).showInfoWindow();
+        }
+
+
+
+//Instantiates a new CircleOptions object +  center/radius
+        CircleOptions circleOptions = new CircleOptions()
+                .center( new LatLng(fence.getLatitude(), fence.getLongitude()) )
+                .radius( fence.getRadius() )
+                .fillColor(0x40ff0000)
+                .strokeColor(Color.TRANSPARENT)
+                .strokeWidth(2);
+
+// Get back the mutable Circle
+        Circle circle = mMap.addCircle(circleOptions);
+// more operations on the circle...
+
+    }
+
+    private void readFromInternalStorage() {
+        ArrayList<GeofenceObjects> returnlist = new ArrayList<>();
+        returnlist = new ArrayList<>();
+        try {
+            FileInputStream fis = openFileInput("GeoFences");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            returnlist = (ArrayList<GeofenceObjects>) ois.readObject();
+            ois.close();
+            System.out.println(returnlist);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        currentList = returnlist;
     }
 
 
